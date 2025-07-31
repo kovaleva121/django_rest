5587a8d20da59e097dce738f8ff768da924c74dd
Ksenia Kovaleva <kovalevaksenia21@gmail.com>
2025-07-31 23:51:14 +0200

    test ci flake8_2 secret key

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-jzrei^51#02m6tulkgq5-r1(@c20arx16)z6hf=797a*6so1hy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
